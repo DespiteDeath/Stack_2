35995f38e97899a34c2b942469045e5d78ed39fd
DespiteDeath <Kogut_nikita@mail.ru>
2017-10-05 23:52:19 +0300

    Update push.cpp

--- examples/push.cpp
+++ examples/push.cpp
@@ -1,5 +1,15 @@
+#include <iostream>
 #include "stack.hpp"
+using namespace std;
+
+void stackPush() {
+	cout << endl << "push.cpp" << endl;
+	Stack<int> arrayInt;
+	for(int i = 0; i < 10; i++) { arrayInt.push(i*2); }
+	for(size_t j = 0; j < arrayInt.count(); j++) { cout << arrayInt[j] << " "; }
+	cout << endl;
+}
 
 int main() {
-	foo_push();
+	stackPush();
 }
