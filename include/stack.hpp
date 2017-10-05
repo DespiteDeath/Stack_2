bd8cf1741361e6c9d15229f5fff7c44e6e58e96c
DespiteDeath <Kogut_nikita@mail.ru>
2017-10-05 23:40:51 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -3,69 +3,63 @@
 
 #include <iostream>
 #include <string.h>
+
 using namespace std;
 
 template <typename T>
 class Stack {
+    T * array_; 
+    size_t array_size_; 
+    size_t count_ = 0; 
+	
 public:
-    Stack();
-    Stack(size_t s);
-    size_t count() const;
-    bool empty() const;
-    void push(T const &);
-    T pop();
-    ~Stack();
-    T& operator [](int spot) {
-    	return array_[spot];
+    	//Конструктор класса без параметров
+    Stack() : array_size_(size_t(10)),
+        array_(new T[10]) {			 //Конструктор по умолчанию
+   }
+    //Конструктор класса с параметром
+    Stack(size_t st) : array_size_(st),
+        array_(new T[st]) { 			//Конструктор с параметром
     }
-private:
-    T* array_;
-    size_t array_size_;
-    size_t _count;
+	
+    T& operator [](int pos) {
+        return array_[pos];
+    }
+	
+    //Метод count
+    size_t count() const {
+        return count_;
+    };
+
+    void push(T const &value) {
+        if (count_ == array_size_) {
+            T* new_array_ = new T[2*array_size_]; 
+            for (int i = 0; i < array_size_; ++i) {
+                new_array_[i] = array_[i]; 
+            }
+            delete[] array_; 
+            array_ = new_array_; 
+        }
+        array_[count_] = value; 
+        ++count_; 
+    }
+
+    T pop() {
+        if (count_ == 0) {
+            throw runtime_error("Stack is empty"); 
+        }
+        --count_; 
+        return array_[count_];
+    }
+    bool empty() const {
+        if(count() == 0) return true;
+        return false;
+   }
+   
+    ~Stack<T>() {
+        delete[] array_; 
+    }
 };
 
-
-template <typename T>
-Stack<T>::Stack() : array_size_(size_t(10)), array_(new T[10]), _count(0) {}
-
-template <typename T>
-Stack<T>::Stack(size_t r) : array_size_(r), array_(new T[r]), _count(0) {}
-
-template <typename T>
-Stack<T>::~Stack() { delete[] array_; }
-
-template <typename T>
-void Stack<T>::push(T const &wow) {
-	if(_count == array_size_) {
-		T* array_exp_ = new T[array_size_*2];
-		for(int i = 0; i < array_size_; i++) { array_exp_[i] = array_[i]; }
-		array_size_ *= 2;
-		delete[] array_;
-		array_ = array_exp_;
-	}
-	array_[_count++] = wow;
-}
-
-template <typename T>
-T Stack<T>::pop() {
-	if(_count == 0) throw runtime_error("Empty_stack");
-	return array_[--_count];
-}
-
-template <typename T>
-size_t Stack<T>::count() const {
-	return _count;
-}
-
-template <typename T>
-bool Stack<T>::empty() const {
-	if(count() == 0) return true;
-	return false;
-}
-
-void foo_init();
-void foo_push();
-void foo_pop();
-void foo_empty();
 
 #endif
