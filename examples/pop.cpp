d3ede6c5d39649eb4b862fdfd1a7f01a68832f25
DespiteDeath <Kogut_nikita@mail.ru>
2017-10-05 23:51:51 +0300

    Update pop.cpp

--- examples/pop.cpp
+++ examples/pop.cpp
@@ -1,5 +1,15 @@
+#include <iostream>
 #include "stack.hpp"
+using namespace std;
+
+void stackPop() {
+	cout << endl << "pop.cpp" << endl;
+	Stack<int> arrayInt;
+	for(int i = 0; i < 10; i++) { arrayInt.push(i); }
+	for(int j = 0; j < 6; j++) { cout << arrayInt.pop() << " "; }
+	cout << endl << endl;
+}
 
 int main() {
-	foo_pop();
+	stackPop();
 }
